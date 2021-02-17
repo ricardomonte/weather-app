bf923c923eb209b5fba61275263ba48abba75e22
ricardo <ramn21@gmail.com>
2021-02-17 22:32:00 +0100

    add styles, change ids, create new div

--- src/assets/weatherGetButton.js
+++ src/assets/weatherGetButton.js
@@ -3,7 +3,7 @@ import getWeather from './getWeather';
 const weatherGetButton = () => {
   const btn = document.querySelector('#submite-player-score');
   btn.addEventListener('click', () => { 
-    const input = document.querySelector('#player-score');
+    const input = document.querySelector('#location-city');
     const units = document.querySelector('#f-or-c');
     console.log(units.value)
     const name = input.value;
