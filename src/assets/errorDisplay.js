0e64fa50fa099ca21eee795096ceb65d5da399bb
ricardo <ramn21@gmail.com>
2021-02-18 00:17:08 +0100

    clean div before append

--- src/assets/errorDisplay.js
+++ src/assets/errorDisplay.js
@@ -4,6 +4,7 @@ const errorDisplay = () => {
   pMessage.textContent = 'Sorry, we could not get the location, please try again';
   display.classList.add('container-current__weather');
   pMessage.classList.add('message-error');
+  display.innerHTML = ""; 
   display.insertAdjacentElement('beforeend', pMessage);
 };
 
