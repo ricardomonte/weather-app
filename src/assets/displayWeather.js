b685633515c789ae97ae614dff0f66dbe4f2a9ec
ricardo <ramn21@gmail.com>
2021-02-17 17:57:20 +0100

    add displayweather function, display result fetch

---
+++ src/assets/displayWeather.js
@@ -0,0 +1,26 @@
+const changeUnit = (unit) => {
+  if(unit === 'metric') {
+    return 'C'
+  } else {
+    return 'F'
+  }
+}
+
+const rounding = (temp) => {
+  return Math.round(temp)
+}
+
+const displayWeather = (temp, location, country, weather, unit) => {
+  const display = document.querySelector('#scores');
+  const pTemp = document.createElement('p');
+  const pCountry = document.createElement('p');
+  const roundedTemp = rounding(temp)
+  const farenCels = changeUnit(unit)
+  pTemp.innerHTML = `Temperature: ${roundedTemp}&deg;${farenCels}`
+  pCountry.textContent = `${location}/${country}`
+  display.innerHTML = ""
+  display.append(pCountry)
+  display.append(pTemp)
+}
+
+export { displayWeather as default }
