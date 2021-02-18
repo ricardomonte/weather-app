e49b975e481fb5575e0f07d3e7f81bda441ec989
ricardo <ramn21@gmail.com>
2021-02-18 17:06:07 +0100

    Add toggle function

--- src/assets/displayWeather.js
+++ src/assets/displayWeather.js
@@ -1,5 +1,25 @@
 import classGenerator from './classGenerator';
 
+const celciusToFahrenheit = (value) => Math.round(value * 1.8 + 32);
+
+const fahrenheitsToCelcius = (value) => Math.round((value - 32) / 1.8);
+
+const toggleMetric = (element) => {
+  element.addEventListener('click', () => {
+    if (element.textContent.includes('C')) {
+      const currentTemp = element.dataset.temp;
+      const fTemp = celciusToFahrenheit(currentTemp);
+      element.textContent = `${fTemp}°F`;
+      element.dataset.temp = fTemp;
+    } else {
+      const currentTemp = element.dataset.temp;
+      const celcius = fahrenheitsToCelcius(currentTemp);
+      element.textContent = `${celcius}°C`;
+      element.dataset.temp = celcius;
+    }
+  });
+};
+
 const changeUnit = (unit) => {
   if (unit === 'metric') {
     return 'C';
@@ -18,6 +38,7 @@ const displayWeather = (temp, location, country, weather, unit) => {
   const farenCels = changeUnit(unit);
   pTemp.innerHTML = `${roundedTemp}&deg;${farenCels}`;
   pCountry.textContent = `${location}/${country}`;
+  pTemp.dataset.temp = roundedTemp;
   pTemp.classList.add('temperature');
   pCountry.classList.add('country');
   display.innerHTML = '';
@@ -26,6 +47,8 @@ const displayWeather = (temp, location, country, weather, unit) => {
   display.append(pCountry);
   display.append(pTemp);
   display.append(contBack);
+
+  toggleMetric(pTemp);
 };
 
 export { displayWeather as default };
